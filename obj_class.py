2260ce696d489ccf35c40cad0c40a87a84aef242
Prethivi1608 <prethiviraj@gmail.com>
2025-03-08 01:16:18 +1100

    first commit

---
+++ obj_class.py
@@ -0,0 +1,40 @@
+import cv2
+def captureImage():
+    camera_name = 0
+    vedioCapture = cv2.VideoCapture(camera_name)
+    
+    num_images = 500
+    file_path = "/media/prethiviraj/R'kived Lab/Projects/2025/Computer vision/images_prethivi/"
+    
+    for i in range(num_images):
+        success,frame = vedioCapture.read()
+    
+        if success == True:
+            file_name = f'{file_path}image_file{i}.jpg'
+            cv2.imwrite(file_name,frame)
+            cv2.waitKey(0)
+                
+    
+    vedioCapture.release()
+
+cv2.destroyAllWindows()
+        
+    # for i in range(num_images):
+    #     file_name = f'{file_path}images_[{i}]'
+    #     cv2.imwrite(file_name,frame)
+    #     cv2.waitKey(0)
+    #     print(f"Saving images {i}")
+    
+
+
+def main():
+    captureImage()
+    
+    
+if __name__ == '__main__':
+    main()
+
+        
+    
+    
+    
